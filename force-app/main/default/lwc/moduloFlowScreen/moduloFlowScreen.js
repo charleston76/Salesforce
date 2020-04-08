6e775f549f9f5c9c4ebc1db84b57e49ed4370b8b
Charleston Costa Santos - CCO <charleston.costa@iteris.local>
2020-04-08 18:01:12 -0300

    Flow

--- force-app/main/default/lwc/moduloFlowScreen/moduloFlowScreen.js
+++ force-app/main/default/lwc/moduloFlowScreen/moduloFlowScreen.js
@@ -1,12 +1,49 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api,wire } from 'lwc';
+import getTreinamento from '@salesforce/apex/TreinamentoController.getTreinamento';
 
 export default class ModuloFlowScreen extends LightningElement {
     @api selectedAccs = [];
     @api selectedAccsString;
     @api Accs = [];
-    @api value = '';
-    @api objOption = [];
+    @api radioValue = '';
 
+    @api objFiltro = {
+        Id: '',
+        Name: '',
+        Descricao: ''
+    };
+
+   @wire(getTreinamento, { strFiltro: JSON.stringify(objFiltro)}  ) 
+   ModulosAtivos;
+
+
+    get radioOptionMock() {
+        return [
+            { label: 'Opção 1', value: 'option1' },
+            { label: 'Opção 2', value: 'option2' },
+        ];
+    }
+
+    get radioOption()  {
+         let optionsValues = [ { label: 'Opção 1', value: 'option1' },
+                             { label: 'Opção 2', value: 'option2' }];
+        let test=[];
+        // console.log('ModulosAtivos optionsValues'); 
+        // //for( let i = 0; i < this.ModulosAtivos.length; i++) {
+        console.log('Antes do for  ' + this.ModulosAtivos);
+        console.log('length  ' + this.ModulosAtivos.length);
+            
+         for( let i = 0; i < optionsValues.length; i++) {
+             console.log('optionsValues ' + i);
+             test.push({
+                 label: optionsValues[i].label,
+                 value: optionsValues[i].value
+             })
+             console.log('test ' + test[i].label);
+        }
+        console.log('saiu ');
+        return test;
+      }
 
     handleCheck(event) {
         if(!this.selectedAccs.includes(event.currentTarget.name))
@@ -22,21 +59,8 @@ export default class ModuloFlowScreen extends LightningElement {
         
     }
 
-        get options() {
-            if(!this.selectedAccs.includes(event.currentTarget.name))
-                this.selectedAccs.push(event.currentTarget.name);
-            else {
-                for(let i = 0; i < this.selectedAccs.length; i++) {
-                    if(event.currentTarget.name === this.selectedAccs[i])
-                    this.selectedAccs.splice(i, 1);
-                }
-            }
-
-            
-            return [
-                { label: 'Sales', value: 'option1' },
-                { label: 'Force', value: 'option2' },
-        ];
-    }    
+    selectRadio(event) {
+        this.selectedAccsString = event.detail.value;
+    }
 
 }
