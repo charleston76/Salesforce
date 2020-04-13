8c2a6d1a7974e80314550a31e344dff984193824
Charleston Costa Santos - CCO <charleston.costa@iteris.local>
2020-04-13 18:01:56 -0300

    FlowBuilder LWC

--- force-app/main/default/lwc/moduloFlowScreen/moduloFlowScreen.js
+++ force-app/main/default/lwc/moduloFlowScreen/moduloFlowScreen.js
@@ -1,8 +1,10 @@
 import { LightningElement, api,wire, track } from 'lwc';
-import getTreinamento from '@salesforce/apex/TreinamentoController.getTreinamento';
+import getModuloAtivoRelacionado from '@salesforce/apex/ModuloController.getModuloAtivoRelacionado';
 
 export default class ModuloFlowScreen extends LightningElement {
     @api varIdTreinamentoLwc;
+    @api varIdModuloLwc;
+    @api varOptionModuloLwc;
     @api selectedAccs = [];
     @api selectedAccsString;
     @api Accs = [];
@@ -10,15 +12,17 @@ export default class ModuloFlowScreen extends LightningElement {
 
     @track objFiltro = {
         Id: '',
+        TreinamentoId: '',
         Name: '',
         Descricao: ''
     };
 
     @track strSearch = JSON.stringify(this.objFiltro);
 
-    @wire(getTreinamento, { strFiltro: '$strSearch' } ) // 
+    @wire(getModuloAtivoRelacionado, { strFiltro: '$strSearch' } ) // 
     wiredResult(data,error) { 
         if(data){
+            console.log('strSearch -->'+this.strSearch);   
             this.objResults= data;
         } else if(error){
             console.log('error -->'+error);   
@@ -29,13 +33,13 @@ export default class ModuloFlowScreen extends LightningElement {
         let vlrReturn=[];
         // Tenho que fazer a tela entender esse cara para carregar os mudolos
         // criar uma nova classe de modulos e mostrar tudo na tela no lugar dos treinamentos
-        console.log('varIdTreinamentoLwc - ' + this.varIdTreinamentoLwc);
-
+        //console.log('varIdTreinamentoLwc - ' + this.varIdTreinamentoLwc);
+        //console.log('objResults.data ' + this.objResults.data);
         if (this.objResults.data != undefined){
              for( var i = 0; i < this.objResults.data.length; i++) {
                  vlrReturn.push({
-                    label: this.objResults.data[i].Name,
-                    value: this.objResults.data[i].Id
+                    label: this.objResults.data[i].ModuloId__r.Name,
+                    value: this.objResults.data[i].ModuloId__r.Id
                 })
             }
         } 
@@ -56,8 +60,20 @@ export default class ModuloFlowScreen extends LightningElement {
         
     }
 
-    selectRadio(event) {
-        this.selectedAccsString = event.detail.value;
+    handleChange(event) {
+        this.varIdModuloLwc = event.detail.value;
+        var Lista = this.radioOption;
+        var optLWC = this.varIdModuloLwc;
+        //this.varOptionModuloLwc = event.detail[event.detail.value].label;
+        var optIndex = Lista.map(function (e) {
+            if (e.value == optLWC) {
+                return e.label;
+            }
+        }).indexOf(Lista.label);
+
+        this.varOptionModuloLwc = Lista[optIndex].label;
+        console.log('varIdModuloLwc ' + this.varIdModuloLwc);
+        console.log('varOptionModuloLwc ' + this.varOptionModuloLwc);
     }
 
 }
